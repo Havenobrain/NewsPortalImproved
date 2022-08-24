ef538615b8533d79363da29377303cb881d11772
Havenobrain <107768179+Havenobrain@users.noreply.github.com>
2022-08-24 15:24:25 +0300

    hello

--- NewsPortal/newapp/views.py
+++ NewsPortal/newapp/views.py
@@ -1,16 +1,33 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
-
+from .filters import PostFilter
 
 class NewsList(ListView):
     model = Post
     ordering = '-dateCreation'
     template_name = 'news.html'
     context_object_name = 'news'
-    paginated_by = 5
+    paginate_by = 5
+
 
 class NewsDetail(DetailView):
     model = Post
     template_name = 'post.html'
     context_object_name = 'post'
     pk_url_kwarg = 'id'
+
+
+class NewsSearchList(NewsList):
+    model = Post
+    template_name = 'search.html'
+    context_object_name = 'search'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
